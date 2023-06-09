5a345e977549ff3cab856703d1efb387fdc08dbb
JatinSingh08 <singhjatin0812@gmail.com>
2023-06-09 18:38:51 +0530

    fix: button border radius

--- src/components/CanvasComponents/ButtonComponent.jsx
+++ src/components/CanvasComponents/ButtonComponent.jsx
@@ -4,10 +4,13 @@ const ButtonComponent = ({button, element}) => {
   const borderRadius =`${element?.borderRadius}px`
   const bgColor = `${element?.bgColor}`
   const buttonStyle = {
+    borderRadius: borderRadius,
     backgroundColor: bgColor
   }
+  // bg-[${bgColor}]
+  // rounded-[${borderRadius}]
   return (
-    <button className={`bg-[${bgColor}] text-slate-50 font-semibold tracking-wider w-full h-full rounded-[${borderRadius}]`}
+    <button className={` text-slate-50 font-semibold tracking-wider w-full h-full `}
     style={buttonStyle}
     >
       {element.text}
