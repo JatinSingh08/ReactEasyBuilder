74834236a480b98ae849f50210e6baf11b7adb8d
JatinSingh08 <singhjatin0812@gmail.com>
2023-06-09 23:44:05 +0530

    Refactor: refactored styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
-
-// You can split your components
 import Editor from "./Editor";
 import { Header } from "./components";
 
 const App = () => {
   return (
     <div className="App">
-      {/* Feel free to delete the header */}
       <Header />
       <Editor />
     </div>
