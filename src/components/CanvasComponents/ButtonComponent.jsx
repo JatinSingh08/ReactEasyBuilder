7ef4b466b3ee15d01a55aecc30c002a265d60003
JatinSingh08 <singhjatin0812@gmail.com>
2023-06-09 18:26:02 +0530

    fix: fixed button bgColor:

--- src/components/CanvasComponents/ButtonComponent.jsx
+++ src/components/CanvasComponents/ButtonComponent.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const ButtonComponent = ({button, element}) => {
-  const borderRadius =`${element.borderRadius}px`
-  const bgColor = `${element.bgColor}`
-  
+  const borderRadius =`${element?.borderRadius}px`
+  const bgColor = `${element?.bgColor}`
+  const buttonStyle = {
+    backgroundColor: bgColor
+  }
   return (
-    <button className={`bg-[${bgColor}] text-slate-50 font-semibold tracking-wider w-full h-full rounded-[${borderRadius}]`}>
+    <button className={`bg-[${bgColor}] text-slate-50 font-semibold tracking-wider w-full h-full rounded-[${borderRadius}]`}
+    style={buttonStyle}
+    >
       {element.text}
     </button>
   )
